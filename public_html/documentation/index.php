c3ab6f723c4029efde46113274cb42695d339836
Matt Harris <mateodelay@gmail.com>
2015-10-26 16:16:41 -0600

    Edited lines 22-23 of paragraph.

--- public_html/documentation/index.php
+++ public_html/documentation/index.php
@@ -19,8 +19,8 @@
 		all abilities from novice to expert.  Non-registered users will be able to search for specific hikes and/or
 		mountain bike rides by area, level of difficulty, amenities, elevation gain, and length (distance).
 		For each hike or ride, they will also be able access trail information uploaded by registered users, including
-		ratings, comments, and photos. They will also see current weather conditions and trail closure updates.  In
-		addition, registered users will be able to arrange meet-ups, set up a current hike/ride group, share information
+		ratings, comments, and photos. They will also see current weather conditions and trail updates. In addition,
+		registered users will be able to arrange meet-ups, set up a current hike/ride group, share information
 		with friends, and build a trail map for future use.  There will also be a section containing Tips, Tricks, and
 		General Information for users wishing to increase their hiking or mountain biking proficiency.
 	</p>
