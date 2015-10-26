5fe4e0e23489056256274de4e8ab6e16e3ebf6df
Matt Harris <mateodelay@gmail.com>
2015-10-26 16:01:45 -0600

    Fixed typos

--- public_html/documentation/index.php
+++ public_html/documentation/index.php
@@ -7,8 +7,10 @@
 	</head>
 	<!-Body for ABQ Open Trails landing page->
 <body>
+	<!-Name of Project->
 	<h1>ABQ Open Trails</h1>
 	<h2>Executive Summary</h2>
+	<!-Text of ABQ Open Trails Executive summary->
 	<p>ABQ Open Trails will be a website for hikers and mountain bikers, used primarily as a way to find trail maps
 		and pertinent information, both official and crowd-sourced, to meet the needs of hikers and mountain bikers of
 		all abilities from novice to expert.  Non-registered users will be able to search for specific hikes and/or
@@ -16,7 +18,8 @@
 		For each hike or ride, they will also be able to read various information including ratings and comments as
 		well as view photos of the trails uploaded by registered users. They will also see current weather conditions and
 		trail closure updates.  In addition, registered users will be able to arrange meet-ups, set up a current hike/ride
-		group, share information with friends, and build a trail map for future use.  There will also be a Tips, Tricks,
-		and General Information section for those wishing to increase their hiking or mountain biking proficiency.
+		group, share information with friends, and build a trail map for future use.  There will also be a section
+		containing Tips, Tricks,and General Information for users wishing to increase their hiking or mountain
+		biking proficiency.
 	</p>
 </body>
