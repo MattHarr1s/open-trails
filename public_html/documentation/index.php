3ab0b6bd63ed4210676bc088283951059d4911af
Matt Harris <mateodelay@gmail.com>
2015-10-26 16:08:40 -0600

    Reworked executive summary paragraph.

--- public_html/documentation/index.php
+++ public_html/documentation/index.php
@@ -15,11 +15,10 @@
 		and pertinent information, both official and crowd-sourced, to meet the needs of hikers and mountain bikers of
 		all abilities from novice to expert.  Non-registered users will be able to search for specific hikes and/or
 		mountain bike rides by area, level of difficulty, amenities, elevation gain, and length (distance).
-		For each hike or ride, they will also be able to read various information including ratings and comments as
-		well as view photos of the trails uploaded by registered users. They will also see current weather conditions and
-		trail closure updates.  In addition, registered users will be able to arrange meet-ups, set up a current hike/ride
-		group, share information with friends, and build a trail map for future use.  There will also be a section
-		containing Tips, Tricks,and General Information for users wishing to increase their hiking or mountain
-		biking proficiency.
+		For each hike or ride, they will also be able access trail information uploaded by registered users, including
+		ratings, comments, and photos. They will also see current weather conditions and trail closure updates.  In
+		addition, registered users will be able to arrange meet-ups, set up a current hike/ride group, share information
+		with friends, and build a trail map for future use.  There will also be a section containing Tips, Tricks,and
+		General Information for users wishing to increase their hiking or mountain biking proficiency.
 	</p>
 </body>
