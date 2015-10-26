8d109c0c50a41a4f62955d0013caac510e2defd1
Matt Harris <mateodelay@gmail.com>
2015-10-26 15:54:55 -0600

    Created basic HTML document with English language and charset utf-8. Created headings for ABQ Open Trails and Executive Summary. Added paragraph of executive summary.

---
+++ public_html/documentation/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<HTML lang="eng>"
+	<!-Header for ABQ Open Trails landing page->
+	<head>
+		<meta charset="UTF-8">
+		<title>ABQ Open Trails</title>
+	</head>
+	<!-Body for ABQ Open Trails landing page->
+<body>
+	<h1>ABQ Open Trails</h1>
+	<h2>Executive Summary</h2>
+	<p>ABQ Open Trails will be an a website for hikers and mountain bikers, used primarily as a way to find trail maps
+		and pertinent information, both official and crowd-sourced, to meet the needs of hikers and mountain bikers of
+		all abilities from novice to expert.  Non-registered users will be able to search for specific hikes and/or
+		mountain bike rides by area, level of difficulty, amenities, elevation gain, and length (distance).
+		For each hike or ride, they will also be able to read various information including ratings and comments as
+		well as view photos of the trails uploaded by registered users. They will also see current weather conditions and
+		trail closure updates.  In addition, registered users will be able to arrange meet-ups, set up a current hike/ride
+		group, share information with friends, and build a trail map for future use.  There will also be a Tips, Tricks,
+		and General Information section for those wishing to increase their hiking or mountain biking proficiency.
+	</p>
+</body>
