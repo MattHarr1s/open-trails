d3146614419c92108acafb2961cfc31eb7bd61b8
Louis Gill <lgill7@cnm.edu>
2015-10-28 15:25:12 -0600

    added Matt's persona

--- public_html/documentation/persona-devin.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Matt
- * Date: 10/28/2015
- * Time: 1:55 PM
- */
--- public_html/documentation/use-cases.php
+++ public_html/documentation/use-cases.php
@@ -5,7 +5,7 @@
 		<title>Use Cases</title>
 	</head>
 	<body>
-		<h1>Freddy's Use Case</h1>
+		<h1>Shawn's Use Case #1</h1>
 		<p>When Freddy first uses the website, he is presented with an option to register an account. He may do so by linking a Facebook or Google+ account, or by providing an email address and setting a password.</p>
 		<p>Freddy clicks the “link with Facebook account” button and is immediately brought to the homepage of the site.</p>
 		<p>From here, Freddy clicks on the “Find a trail” button. He uses advanced search options to display only the trails listed as “high difficulty”. He then sorts these trails by length by clicking on the head of the “length” column.</p>
@@ -14,5 +14,8 @@
 		<p>Freddy scrolls down on the “Dead Man’s Pass” page and begins to read comments left by other users. One reads:  “Beware, ye who tread here”. Freddy clicks the “reply” button located within this user’s comment box, which displays an input field. Freddy then types “Challenge accepted!” into it and submits it by clicking an “OK” button.</p>
 		<p>Freddy continues to scroll down the page until he happens upon a comment which says “this trail sux!” Freddy clicks a tiny down arrow located within the comment box, which lowers its relevancy. This is reflected by a number located within the comment box and has now been decreased by 1, to -1. Because this comment now has the lowest rating of all the comments on the page, it immediately drops to the bottom of the page.</p>
 		<p>Freddy then scrolls back to the top of the page and clicks a “+” which allows him to zoom in on the map. After studying it sufficiently he clicks the drop-down menu in the upper-right hand corner of the screen and selects the “sign out” option.</p>
+		<hr>
+		<h1></h1>
+		<p></p>
 	</body>
 </html>
