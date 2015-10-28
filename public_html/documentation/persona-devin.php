9c69e8c85a6a5c1ccf58b124e25e3c0bf2d02d10
Matt Harris <mateodelay@gmail.com>
2015-10-28 15:00:00 -0600

    Created Devin

--- public_html/documentation/persona-devin.php
+++ public_html/documentation/persona-devin.php
@@ -1,7 +1,51 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Matt
- * Date: 10/28/2015
- * Time: 1:55 PM
- */
+<!DOCTYPE HTML>
+<!-set language to English->
+<html lang="en">
+	<!-Header elements for page->
+	<head>
+		<meta charset="UTF-8">
+		<title>Dangerous Devin</title>
+	</head>
+	<!-body of persona page for Devin->
+	<body>
+		<!- Heading one "Dangerous Devin"->
+		<h1>Dangerous Devin</h1>
+		<!-Paragraph explaining Devin's persona and use case scenarios->
+		<p>Devin is a 32 year old single male who lives life by nobody's rules but his own. A man of many hats,
+			Devin is constantly looking for a new challenge, and a new opportunity to talk about it. Devin is always on his
+			Droid Turbo sharing photos from the the diverse locals he is often found at, such as a movie set,
+			a wrestling tournament or a brutal hike. Devin broadcasts his adventures from two primary devices:</p>
+
+		<!-List of Devin's Devices>
+		<ol>
+			<!-Droid Turbo Primary device for broadcasting his adventures->
+			<li>Droid Turbo 32 GB</li>
+			<ul>
+				<li>Screen size: 5.2 inch screen</li>
+				<li>Screen resolution: 2560 x 1440 Quad HD 565 ppi</li>
+				<li>Storage capacity: 32 GB</li>
+				<li>GSM Radio: 4G LTE</li>
+				<li>Carrier: Exclusively on Verizon</li>
+				<li>OS: Android 5.1 Lollipop</li>
+			</ul>
+			<p>Devin lives on his Droid Turbo. The 5.2 inch Quad HD screen contains all the real estate necessary to make
+				sure his snaps, instagrams and facebook posts look as awesome as he thinks they are. The combination of
+				size and power in the Droid Turbo almost guarantees Devin does everything from it.Devin knows Google Now
+				and often uses it to find a happening night club, public transportation or on good nights, both. The Droid
+				Turbo has the power Devin needs for most of his computing tasks
+				have information at glance, without unexpected hiccups keeping him from the knowledge he craves.</p>
+			<!-HP Envy Ultrabook  Secondary Device used for web surfing, social media, banking, travel, shopping as well
+			as document creation and book keeping->
+			<li>HP Envy Ultrabook  14.1"</li>
+			<ul>
+				<li>Intel Core i5 1.6GHz</li>
+				<li>14.1" 1280x720 LED Backlit Display</li>
+				<li>500GB HDD and 32 SSD Flash Storage</li>
+				<li>Windows 7</li>
+			</ul>
+			<p>When his Droid Turbo just isn't powerful enough for the task, Devin opens up his HP Envy Ultrabook and performs
+				serious work on the 14.1 inch HD display. The core i5 1.6ghz processor is plenty powerful for the times
+				Devin needs to edit photos, update his financial spreadsheets, or create a sample portfolio for a
+				potential client. Devin also enjoys surfing the web and learning about new hobbies from the beautiful
+				1440 x 900 LED Backlit Display.</p>
+		</ol>
