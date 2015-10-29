6228424c9c048f359b3fede1900faf31551fc5c7
Matt Harris <mateodelay@gmail.com>
2015-10-29 09:43:26 -0600

    Applied formatting to lines 8-17

--- public_html/documentation/persona-matt.php
+++ public_html/documentation/persona-matt.php
@@ -5,6 +5,15 @@
 		<title>Matt's Persona</title>
 	</head>
 	<body>
-		<p>Matt Gregory is a 56 year old Hispanic engineer at Sandia National Labs who enjoys strenuous hikes on the weekend with his wife.  He leads hikes for OSHA.  He is very computer savvy and uses Windows 10 on his laptop and home desktop computers.  He uses Facebook to keep up with friends and family, but does not use Twitter or Instagram.  He prefers to figure things out for himself and does not ask for help.  When he hikes, he often uses a GPS hiking tracker and the MapMyHike app on his Samsung smartphone (running Android Lollipop) when his tracker is unavailable.  In the summer, he will go out and hike for 4-6 hours at a time.  In the winter, or on very windy days in the spring, he will go out only for 1-2 hours.  He also spends some weekends backpacking from Spring to Fall.  He prefers hiking trails with scenic views and unusual plants away from crowds and off the beaten path.  He is a registered user of the site.</p>
+		<p>Matt Gregory is a 56 year old Hispanic engineer at Sandia National Labs who enjoys strenuous hikes on the
+			weekend with his wife.  He leads hikes for OSHA.  He is very computer savvy and uses Windows 10 on his laptop
+			and home desktop computers.  He uses Facebook to keep up with friends and family, but does not use Twitter or
+			Instagram.  He prefers to figure things out for himself and does not ask for help.  When he hikes, he often
+			uses a GPS hiking tracker and the MapMyHike app on his Samsung smartphone (running Android Lollipop) when his
+			tracker is unavailable.  In the summer, he will go out and hike for 4-6 hours at a time.  In the winter, or on
+			very windy days in the spring, he will go out only for 1-2 hours.  He also spends some weekends backpacking
+			from Spring to Fall.  He prefers hiking trails with scenic views and unusual plants away from crowds and off
+			the beaten path.  He is a registered user of the site.
+		</p>
 	</body>
 </html>
