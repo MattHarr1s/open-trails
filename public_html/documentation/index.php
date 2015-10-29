813ddba7ffd9fa19802298376809a8f0c98f552a
Matt Harris <mateodelay@gmail.com>
2015-10-29 09:39:02 -0600

    Created persona section and linked to the persona pages.

--- public_html/documentation/index.php
+++ public_html/documentation/index.php
@@ -1,33 +1,48 @@
 <!DOCTYPE HTML>
 <HTML lang="eng>"
-	<!-Header for ABQ Open Trails landing page->
+	<!-Header for Trail Quail landing page->
 	<head>
 		<meta charset="UTF-8">
-		<title>ABQ Open Trails</title>
+		<title>Trail Quail</title>
 	</head>
-	<!-Body for ABQ Open Trails landing page->
+	<!-Body for Trail Quail landing page->
 	<body>
 		<!-Name of Project->
-		<h1>ABQ Open Trails</h1>
+		<h1>Trail Quail</h1>
 		<!-Executive Summary->
-		<h2>Executive Summary</h2>
-		<!-Text of ABQ Open Trails Executive summary->
-
-	<p>ABQ Open Trails will be a website for hikers and mountain bikers, used primarily as a way to find trail maps
-		and pertinent information, both official and crowd-sourced, to meet the needs of hikers and mountain bikers of
-		all abilities from novice to expert.  Non-registered users will be able to search for specific hikes and/or
-		mountain bike rides by area, level of difficulty, amenities, elevation gain, and length (distance).
-		For each hike or ride, they will also be able access trail information uploaded by registered users, including
-		ratings, comments, and photos. They will also see current weather conditions and trail updates. In addition,
-		registered users will be able to arrange meet-ups, set up a current hike/ride group, share information
-		with friends, and build a trail map for future use.  There will also be a section containing Tips, Tricks, and
-		General Information for users wishing to increase their hiking or mountain biking proficiency.
-	</p>
-
+			<div>
+				<h2>Executive Summary</h2>
+						<!-Text of Trail Quail Executive summary->
+							<p>Trail Quail will be a website for hikers and mountain bikers, used primarily as a way to find trail maps
+			and pertinent information, both official and crowd-sourced, to meet the needs of hikers and mountain bikers of
+			all abilities from novice to expert.  Non-registered users will be able to search for specific hikes and/or
+			mountain bike rides by area, level of difficulty, amenities, elevation gain, and length (distance).
+			For each hike or ride, they will also be able access trail information uploaded by registered users, including
+			ratings, comments, and photos. They will also see current weather conditions and trail updates. In addition,
+			registered users will be able to arrange meet-ups, set up a current hike/ride group, share information
+			with friends, and build a trail map for future use.  There will also be a section containing Tips, Tricks, and
+			General Information for users wishing to increase their hiking or mountain biking proficiency.
+							</p>
+			</div>
+			<!-Personas->
+			<div>
+				<h2>Personas</h2>
+					<p>Trail Quail will be accessible by anyone interested in hiking or biking, regardless of experience level.
+					Trail Quail's UX has been designed around the following Personas:
+					</p>
+					<ul>
+						<li><a href="persona-maria.php">Maria, 53 F, Parks and Recreation Employee </a></li>
+						<li><a href="persona-matt.php">Matt, 56 M, Engineer</a></li>
+						<li><a href="persona-shawn.php.php">Shawn, 38 M, Financial Adviser</a></li>
+					</ul>
+			</div>
 	</body>
 	<footer>
 		<!-Follow @Trail_Quail on Twitter->
 		<a href="https://twitter.com/Trail_Quail" class="twitter-follow-button" data-show-count="false" data-size="large">
 			ollow @Trail_Quail</a>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-	</footer>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if
+			(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+			fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+		</script>
+	</foot
