b691c95d645925932e1095de73dcdd976a2d31a7
Matt Harris <mateodelay@gmail.com>
2015-10-29 09:51:07 -0600

    Fixed twitter button

--- public_html/documentation/index.php
+++ public_html/documentation/index.php
@@ -38,11 +38,10 @@
 			</div>
 	</body>
 	<footer>
-		<!-Follow @Trail_Quail on Twitter->
 		<a href="https://twitter.com/Trail_Quail" class="twitter-follow-button" data-show-count="false" data-size="large">
 			Follow @Trail_Quail</a>
-		<script>function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if
-			(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
-			fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if
+			(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.
+				parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 		</script>
 	</footer>
