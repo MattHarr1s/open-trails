21b7ef336355464445f54ba8c12dbf1b08b347df
Matt Harris <mateodelay@gmail.com>
2015-11-03 21:26:32 -0700

    Fixed formatting on Maria's use case 1

--- public_html/documentation/use-cases.php
+++ public_html/documentation/use-cases.php
@@ -7,13 +7,13 @@
 	<body>
 		<h1>Shawn's Use Case #1</h1>
 		<ul>
-		<li>When Shawn first uses the website, he is presented with an option to register an account. He may do so by linking a Facebook or Google+ account, or by providing an email address and setting a password.</li>
-		<li>Shawn clicks the "link with Facebook account" button and is immediately brought to the homepage of the site.</li>
-		<li>From here, Shawn clicks on the "Find a trail" button. He uses advanced search options to display only the trails listed as "high difficulty". He then sorts these trails by length by clicking on the head of the "length" column.</li>
-		<li>At this point, the system is displaying a list of "high difficulty" trails, the first of which is the longest. It is named "Dead Man's Pass".</li>
-		<li>Shawn clicks on "Dead Man's Pass" and is taken to a page containing a map of the trail and its current weather conditions.</li>
-		<li>Shawn then scrolls back to the top of the page and clicks a "+" which allows him to zoom in on the map. After studying it sufficiently he clicks the drop-down menu in the upper-right hand corner of the screen and selects the "sign out" option.</li>
-			</ul>
+			<li>When Shawn first uses the website, he is presented with an option to register an account. He may do so by linking a Facebook or Google+ account, or by providing an email address and setting a password.</li>
+			<li>Shawn clicks the "link with Facebook account" button and is immediately brought to the homepage of the site.</li>
+			<li>From here, Shawn clicks on the "Find a trail" button. He uses advanced search options to display only the trails listed as "high difficulty". He then sorts these trails by length by clicking on the head of the "length" column.</li>
+			<li>At this point, the system is displaying a list of "high difficulty" trails, the first of which is the longest. It is named "Dead Man's Pass".</li>
+			<li>Shawn clicks on "Dead Man's Pass" and is taken to a page containing a map of the trail and its current weather conditions.</li>
+			<li>Shawn then scrolls back to the top of the page and clicks a "+" which allows him to zoom in on the map. After studying it sufficiently he clicks the drop-down menu in the upper-right hand corner of the screen and selects the "sign out" option.</li>
+		</ul>
 			<hr>
 		<h1>Matt's Use Case #1</h1>
 		<p>Matt goes to quailtrail.net</p>
@@ -29,18 +29,42 @@
 		</ul>
 		<hr>
 		<h1>Maria's Use Case #1</h1>
-		<p>Most of the time, Maria uses the site like other users. However, as one of the designated site administrators (database babysitter or DBB), she checks the site to see if the trail information is correct for safety purposes.  She goes to the homepage and searches for a specific trail by name or location.  A second page brings up a list of hiking or mountain bike trails that meet the criteria she has entered into the search form on the homepage.  Maria selects the trail she wants to check and looks over the map and other information about the trail (users comments, photos, and GPS tracker maps).  As a DBB, she will have an Check Trail option that will put up two windows.  Window 1 will have the official trail information and window 2 will put up information uploaded by registered users.  She will check to make sure the site route data matches the actual trail.  She will also check to see if other key features such as starting location, distance, elevation, elevation gain, accessibility, amenities, and special conditions are correct.  She will make corrections/updates as needed.  She will field trails comments/concerns flagged for updating the database and decide whether or not to incorporate the changes into the site database. Maria will then discard uploaded information deemed unuseful.  Last, she will close the the Check Trail windows and read over the corrected entry.  If the information is correct, she will exit the site or close the browser window.  If not, she will reopen the Check Trail windows to make further corrections.</p>
+			<p>Most of the time, Maria uses the site like other users.
+				However, as one of the designated site administrators (database babysitter or DBB),
+				she checks the site to see if the trail information is correct for safety purposes. To accomplish this task,
+				Maria performs the following tasks:
+			</p>
+		<ul>
+			<li>She goes to the homepage and searches for a specific trail by name or location.	</li>
+			<li>A second page brings up a list of hiking or mountain bike trails that meet the criteria she has entered
+				into the search form on the homepage.</li>
+			<li>Maria selects the trail she wants to check and looks over the map and other information
+				about the trail (users comments, photos, and GPS tracker maps).</li>
+			<li>As a DBB, she will have an Check Trail option that
+				will put up two windows.  Window 1 will have the official trail information and window 2 will put up information
+				uploaded by registered users. </li>
+			<li>She will check to make sure the site route data matches the actual trail.</li>
+			<li>She will also check to see if other key features such as starting location, distance, elevation, elevation gain,
+				accessibility, amenities, and special conditions are correct.</li>
+			<li>She will make corrections/updates as needed.
+				She will field trails comments/concerns flagged for updating the database and decide whether or not to incorporate
+				the changes into the site database.</li>
+			<li>Maria will then discard uploaded information deemed unuseful.</li>
+			<li>Last, she will close the the Check Trail windows and read over the corrected entry.</li>
+			<li>If the information is correct,she will exit the site or close the browser window.
+				If not, she will reopen the Check Trail windows to make further corrections.</li>
+		</ul>
 		<hr>
 		<h1>Maria's Use Case #2</h1>
 		<ul>
-		<li>Maria gets an alert about a trail closure, or community sponsored event, and goes to (opentrails).com</li>
-		<li>The browser then opens  the site for her.</li>
-		<li>Maria then logs into her account(which gives her DbB (databaseBabysitter) privileges.</li>
-		<li>The website verifies her login information and grants DbB privledges.</li>
-		<li>Maria then goes to the website menu and clicks upload.</li>
-		<li>The website then verifies again that maria is DBB and opens the upload menu.</li>
-		<li>Maria selects upload alert and chooses the file she wants to upload.</li>
-		<li>The website verifies that the file contains no malicious content and uploads the file to the alerts tab.</li>
+			<li>Maria gets an alert about a trail closure, or community sponsored event, and goes to (opentrails).com</li>
+			<li>The browser then opens  the site for her.</li>
+			<li>Maria then logs into her account(which gives her DbB (databaseBabysitter) privileges.</li>
+			<li>The website verifies her login information and grants DbB privledges.</li>
+			<li>Maria then goes to the website menu and clicks upload.</li>
+			<li>The website then verifies again that maria is DBB and opens the upload menu.</li>
+			<li>Maria selects upload alert and chooses the file she wants to upload.</li>
+			<li>The website verifies that the file contains no malicious content and uploads the file to the alerts tab.</li>
 		</ul>
 	</body>
 </html>
