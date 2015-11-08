dbd6f5301c1d0dac1264eb48bb58ed9aaf7a1574
Matt Harris <mateodelay@gmail.com>
2015-11-08 15:43:36 -0700

    Constructor for segment objects, lines 63-79, accessor for segmentId lines 87-89, mutator for segmentId started line 99  https://app.asana.com/0/61036105436324/63522485236320

--- public_html/php/classes/segment.php
+++ public_html/php/classes/segment.php
@@ -50,6 +50,53 @@ class Segment {
 	 creates a new segment object which inherits the traits of the segment class
 	 object contains the 4 attributes listed above in the parent class.
 	 *
-	 */
-
+	 * @param mixed $segmentId id of this  segment or null if new segment
+	 * @param int $elevation elevation of the segment in feet, or null if unavailable.
+	 /matttodo/figure out lat and longitude/
+	 * @param $start lattidue and longitude of start
+	 * @param $stop  lattitude and longitude of stop
+	 * @throws InvalidArgumentException if datatypes are not valid
+	 * @throws RangeException if data values are out of bounds (e.g. string insted of int, string too long)
+	 * @throws Exception if some other exception is thrown
+	 *
+	 **/
+	public function __construct($newSegmentId, $newElevation, $newStart, $newStop) {
+		try{
+			$this->setSegmentId($newSegmentId);
+			$this->setElevation($newElevation);
+			$this->setStart($newStart);
+			$this->setStop($newStop);
+	}catch(invalidArgumentException $invalidArgument){
+			// rethrow the exception to the caller
+			throw(new InvalidArgumentException($invalidArgument->getMessage(),0,$invalidArgument));
+	}catch(RangeException $range){
+			//rethrow the exception to the caller
+			throw(new RangeException($range->getMessage(),0,$range));
+	}catch(Exception $exception){
+			//rethrow generic exception
+			throw(new Exception($exception->getMessage(),0,$exception));
+		}
+}
+/**
+ * accessor method for segmentId
+ *
+ * gains access to segmentId for use by mutator method
+ *
+ *@return mixed value of segmentId
+**/
+	public function getSegmentId() {
+		return $this->segmentId;
+	}
+/**
+ * mutator method for segmentId
+ *
+ * modifies values of segmentId using the access given by the accessor method.
+ *
+ * @param mixed $newSegmentId new value of segmentId
+ * @throws InvalidArgumentException if $newSegmentId is not an integer
+ * @throws RangeException if $newSegmentId is not positive
+ **/
+	public function setSegmentId($segmentId) {
+		$this->segmentId = $segmentId;
+	}
 }
