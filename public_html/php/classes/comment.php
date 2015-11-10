9f8cb7db51a0acbb84f76f3b7942f65b4c9a2e4f
George Kephart <Gkephart@cnm.edu>
2015-11-10 11:08:32 -0700

    fixed a few small mistakes in the mysql schema. https://app.asana.com/0/61036105436346/63522490997546

--- public_html/php/classes/comment.php
+++ public_html/php/classes/comment.php
@@ -179,6 +179,7 @@ class comment {
 	 *
 	 * @param string $newCommentPhoto new value of photo file extension
 	 * @throws InvalidArgumentException if $newCommentPhoto is not a string or insecure
+	 * @throws RangeException
 	 */
 
 
