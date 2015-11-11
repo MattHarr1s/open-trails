1a73c1efa4ed90f561458efd3d48c8775df760f3
Matt Harris <mateodelay@gmail.com>
2015-11-11 15:50:16 -0700

    Created accessors and mutators for x and y in point class.  https://app.asana.com/0/61036105436324/63522485236320

--- public_html/php/classes/point.php
+++ public_html/php/classes/point.php
@@ -8,6 +8,8 @@ require_once(dirname(dirname(__DIR__)) . "public_html/php/classes/autoload.php")
  * var $x is the longitude
  * var $y is the latitude
  *
+ * @author Trail Quail
+ * @author Matt Harris
  */
 class Point {
 	/**
@@ -55,30 +57,25 @@ class Point {
 	 * @param double $newX
 	 * @throws RangeException
 	 * @throws InvalidArgumentException
-	 * @return double
 	 **/
 	public function setX($newX) {
-		//make sure the int is not null
-		if($newX === null){
-			throw(new InvalidArgumentException("longitude cannot be null"));
-		}
-
-		//make sure the int in in the proper range
-		if($newX <= -180){
-			throw(new RangeException("longitude must be greater than -180"));
-		}
-
-		if($newX >= 180){
-			throw(new RangeException("longitude must be less than or equal to 180"));
-		}
-		// verify the new int
-		$newX = filter_var($newX, FILTER_VALIDATE_FLOAT);
+		// verify the new float
+		$newX = $newX == 0.0 ? 0.0 : filter_var($newX, FILTER_VALIDATE_FLOAT);
 		if($newX === false){
 			throw(new InvalidArgumentException("longitude is not a valid double"));
 		}
 
-		//convert and return the new int
-		return(doubleval($newX));
+		//make sure the float in in the proper range
+		if($newX < -180){
+			throw(new RangeException("longitude must be greater than -180"));
+		}
+
+		if($newX > 180){
+			throw(new RangeException("longitude must be less than or equal to 180"));
+		}
+
+		//convert and return the new float
+		$this->x = $newX;
 	}
 
 	/**
@@ -93,27 +90,25 @@ class Point {
 	 * @param double $newY
 	 * @throws InvalidArgumentException
 	 * @throws RangeException
-	 * @return double
+
 	**/
 	public function setY($newY){
-		// make sure the int is not null
-		if ($newY === null){
-			throw(new InvalidArgumentException("latitude cannot be null" ));
+		// verify the new float
+		$newY = $newY == 0.0 ? 0.0: filter_var($newY,FILTER_VALIDATE_FLOAT);
+		if($newY === false){
+			throw(new InvalidArgumentException("latitude is not a valid double"));
 		}
 
-		//make sure the int is in the proper range
-		if($newY <= -90){
+		//make sure the float is in the proper range
+		if($newY < -90){
 			throw(new RangeException("latitude must be equal to or greater than -90"));
 		}
 
-		if($newY >= 90){
+		if($newY > 90){
 			throw(new RangeException("latitude must be equal to less than 90"));
 		}
 
-		//verify the new int
-		$newY = filter_var($newY,FILTER_VALIDATE_FLOAT);
-		if ($newY === false){
-			throw(new InvalidArgumentException("latitude is not a valid double"));
-		}
+		// convert and return the new float
+		$this->y = $newY;
 	}
 }
