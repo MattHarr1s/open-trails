cb1067237c0e4d89e7045234ec9325face399130
Matt Harris <mateodelay@gmail.com>
2015-11-15 19:12:01 -0700

    Created variables for the unit test https://app.asana.com/0/61036105436324/63522084146088

--- public_html/php/test/trail-test.php
+++ public_html/php/test/trail-test.php
@@ -11,15 +11,93 @@ require_once(dirname(__DIR__). "/classes/trail.php");
  * @see Trail
  * @author Matt Harris <mattharr505@gmail.com> and Trail Quail <trailquailabq@gmail.com>
 **/
-class TrailTest extends TrailQuailTest {
+class TrailTest extends TrailQuailTest{
 	/**
 	 * valid trailId to use
 	 * @var int $VALID_TRAILID
-	 *
-	**/
+	 */
+
 	protected $VALID_TRAILID ="5";
+	/**
+	 * id for the content of the submission of the trail object
+	 * @var int $VALID_SUBMITTRAILID
+ **/
+	protected $VALID_SUBMITTRAILID ="7";
 
 	/**
-	 *
-	**/
+	 * id of user that submits to the trail
+	 * @var int $VALID_USERID
+	 **/
+	protected $VALID_USERID="2";
+
+	/**
+	 * accessibility info for trail
+	 * @var string $VALID_TRAILACCESSIBILITY
+	 **/
+	protected $VALID_TRAILACCESSIBILITY ="Wheelchair";
+
+	/**
+	 *information on amenities on trail
+	 * @var string $VALID_TRAILAMENITIES
+	 */
+	protected $VALID_TRAILAMENITIES = "Picnic area";
+	/**
+	 * information on the trail condition
+	 * @var string $VALID_TRAILCONDITION
+	 **/
+	protected $VALID_TRAILCONDITIION = "Good";
+
+	/**
+	 * information describing the trail
+	 * @var string $VALID_TRAILDESCRIPTION
+	 **/
+	protected $VALID_TRAILDESCRIPTION = "This trail is a beautiful winding trail located in the Sandia Mountains";
+
+	/**
+	 * difficulty rating of trail
+	 * @var int $VALID_TRAILDIFFICULTY
+	 **/
+	protected $VALID_TRAILDIFFICULTY = "3";
+
+	/**
+	 * length of the trail
+	 * @var int $VALID_TRAILDISTANCE
+	 **/
+	protected $VALID_TRAILDISTANCE = "1000";
+
+	/**
+	 * type of submission made to trail
+	 * @var int $VALID_TRAILSUBMISSIONTYPE
+	 **/
+	protected $VALID_TRAILSUBMISSIONTYPE = "2";
+
+	/**
+	 * type of terrain on the trail
+	 * @var string $VALID_TRAILTERRAIN
+	 **/
+	protected $VALID_TRAILTERRAIN = "Mostly switchbacks with a few sections of rock fall";
+
+	/**
+	 * name of trail
+	 * @var string $VALID_TRAILNAME
+	 **/
+	protected $VALID_TRAILNAME = "La Luz";
+
+	/**
+	 *amount of traffic on trail
+	 *@var string $VALID_TRAILTRAFFIC
+	 **/
+	protected $VALID_TRAILTRAFFIC = "Heavy";
+
+	/**
+	 * main use of the trail (hiking, cycling, skiing)
+	 * @var string $VALID_TRAILUSE
+	 **/
+	protected $VALID_TRAILUSE = "Hiking";
+
+	/**
+	 * id for the submission on the trail object. Exists so the primary key does not have to get updated.
+	 * @var string $VALID_TRAILUUID
+	 **/
+	protected $VALID_TRAILUUID = "SSEERFFV4444554";
 }
