23e30aa84fcaa3fa24f9468a7a4176333d3e6b58
Matt Harris <mateodelay@gmail.com>
2015-11-15 22:20:54 -0700

    Created segment-test class https://app.asana.com/0/61036105436324/63522485236320

---
+++ public_html/php/test/segment-test.php
@@ -0,0 +1,16 @@
+<?php
+require_once("segment.php");
+require_once(dirname(__DIR__). "/classes/segment.php");
+
+/**
+ *
+ * Full PHPUnit test for the Segment class
+ *
+ * This is a complete PHPunit test of the Segment class. It is complete because *ALL* mySQL/PDO enabled methods are tested for both invalid and valid inputs.
+ *
+ * @see Segment
+ * @author Matt Harris <mattharr505@gmail.com> and Trail Quail <trailquailabq@gmail.com>
+ **/
+class SegmentTest extends TrailQuailTest{
+
+}
