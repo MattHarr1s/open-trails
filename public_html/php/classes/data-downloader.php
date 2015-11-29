d8c4339134bfe1e7d13df3e26be36f7fef3c6f41
Matt Harris <mateodelay@gmail.com>
2015-11-28 19:21:39 -0700

    https://app.asana.com/0/61036105436324/69245087519157 Starting work on the data downloader.

---
+++ public_html/php/classes/data-downloader.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once "autoload.php";
+require_once("/ect/apache2/mysql/encrypted-config.php");
+
+/**
+ * This class will download date from the City of Albuquerque Open-Trails Database.
+ * This class can be modified to grab data from the databases of other cities using the open-trails specification.
+ *
+ * @author Matt Harris mattharr505@gmail.com
+ *
+**/
+class DataDownloader{
+
+	/**
+	 * named trails:http://data.cabq.gov/community/opentrails/named_trails.csv
+	 * stewards: http://data.cabq.gov/community/opentrails/stewards.csv
+	 * trail segments: http://data.cabq.gov/community/opentrails/trail_segments.geojson
+	 *
+	**/
+
+}
+/**
+ * Created by PhpStorm.
+ * User: Garage
+ * Date: 11/28/2015
+ * Time: 7:05 PM
+ */
