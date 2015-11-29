5131862ffdeea9f9836732691471350b8c2ae826
Matt Harris <mateodelay@gmail.com>
2015-11-28 19:44:30 -0700

    https://app.asana.com/0/61036105436324/69245087519157 Created getMetaData function.

--- public_html/php/classes/data-downloader.php
+++ public_html/php/classes/data-downloader.php
@@ -19,10 +19,53 @@ class DataDownloader{
 	 *
 	**/
 
+	/**
+	 * Gets the metadata from a file url
+	 *
+	 * @param string %url to grab from
+	 * @param int $redirect whether to redirect or not
+	 * @return mixed stream data
+	 * @throws Exception if file doesn't exist.
+	**/
+	public static function getMetaData ($url, $redirect = 1) {
+		$context = stream_context_create(array("http" => array("follow_location" => $redirect, "ignore_errors" => true, "method" => "HEAD")));
+
+		//"@" suppresses warnings and errors
+		$fd = @fopen($url, "rb", false, $context);
+
+		//grab the stream data
+		$streamData = stream_get_meta_data($fd);
+
+		fclose($fd);
+
+		$wrapperData = $streamData["wrapper_data"];
+
+		//loop through and find the "HTTP" atribute
+		$http ="";
+		foreach ($wrapperData as $data) {
+			if(strpos($data, "HTTP") !== false){
+				$http = $data;
+				break;
+			}
+		}
+
+		if(strpos($http, "400")) {
+			throw (new Exception("Bad request"));
+		}
+		if(strpos($http, "401")) {
+			throw (new Exception ("Unauthorized"));
+		}
+		if(strpos($http, "403")) {
+			throw (new Exception ("Forbidden"));
+		}
+		if(strpos($http, "404")) {
+			throw (new Exception("Not Found"));
+		}
+		if(strpos($http,"418")) {
+			throw(new Exception("Get your tea set"));
+		}
+
+		return $streamData;
+	}
+
 }
-/**
- * Created by PhpStorm.
- * User: Garage
- * Date: 11/28/2015
- * Time: 7:05 PM
- */
