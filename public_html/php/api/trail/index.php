5e97241b945b1cbdad656f43dba9f860a71adc06
Louis Gill <lgill7@cnm.edu>
2015-12-02 14:50:56 -0700

    Fixed stoopid errors https://app.asana.com/0/60997641568227/69245087519159

--- public_html/php/api/trail/index.php
+++ public_html/php/api/trail/index.php
@@ -4,7 +4,7 @@
 require_once dirname(dirname(__DIR__)) . "/classes/autoload.php";
 require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
 // go over with dylan to make sure this is correct
-require_once("etc/apache2/capstone-mySQL/encrypted-config.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 /**
  * controller/api for the trail class
@@ -21,6 +21,11 @@ require_once("etc/apache2/capstone-mySQL/encrypted-config.php");
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
+
+// start the session
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
 
 try{
 	//grab the mySQL connection get correct path from dylan
