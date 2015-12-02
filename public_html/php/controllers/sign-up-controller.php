6725f4571ff541e41e2758b22c4adc2411e80700
Louis Gill <lgill7@cnm.edu>
2015-12-02 14:26:59 -0700

    f

--- public_html/php/controllers/sign-up-controller.php
+++ public_html/php/controllers/sign-up-controller.php
@@ -8,10 +8,10 @@
  **/
 
 //auto loads classes
-require_once(dirname(__DIR__) . "/php/classes/autoload.php");
+require_once dirname(dirname(__DIR__)) . "/php/classes/autoload.php";
 
 //security w/ NG in mind
-require_once(dirname(dirname(__DIR__)) . "/php/lib/xsrf.php");
+require_once dirname(dirname(__DIR__)) . "/php/lib/xsrf.php";
 
 //a security file that's on the server created by Dylan
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
