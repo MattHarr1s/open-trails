ba23f68910bcd18c6b762a352e7bf9b40023d628
Jeff Saul <scaleup13@gmail.com>
2015-12-02 15:49:05 -0700

    adjustments for practice using bootstrap https://app.asana.com/0/61036105436352/63522445646889

---
+++ public_html/php/template/landing-template.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<!-- The 3 meta tags *must* come first in the head; any other head content must come *after* these tags  		-->
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"
+
+		<!-- Bootstrap latest compiled and minified CSS		-->
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+
+		<!-- Optional Bootstrap Theme -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type=""text/css"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!-- [if it IE 9]>
+			<script src="//css.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="//css.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<! [endif]-->
+
+		<!-- jQuery (required for Bootstrap's JS plugins) -->
+		<script src="ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+		<!-- Bootstrap latest compiled and minified Javascript -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"  integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc= sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+
+		<title>Basic Bootstrap Wireframe Exercise</title>
+	</head>
+
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<header>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<!-- begin navigation bar -->
+							<nav class="navbar navbar-inverse">
+
+								<!-- logo and mobile toggle button get grouped together -->
+								<div class="navbar-header">
+									<button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#my-navbars" aris-expanded="false">
+										<span class="sr-only">Main Menu</span>
+										<span class="glyphicon glyphicon-plus"></span>
+									</button>
+									<a href="#" class="navbar-brand">
+										<span class="glyphicon" glyphicon-equalizer="equalizer"></span>
+
+									</a>
+								</div>
+
+							</nav>
+						</div>
+					</div>
+				</div>
+			</header>
+		</div>
+	</body>
+
+</html>
