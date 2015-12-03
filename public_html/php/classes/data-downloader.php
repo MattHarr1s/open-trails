de8abf14e277e2344022318894f8c90d4d4f0264
Matt Harris <mateodelay@gmail.com>
2015-12-02 17:00:24 -0700

    https://app.asana.com/0/61036105436324/69245087519157 working on  read geoJson function line 268

--- public_html/php/classes/data-downloader.php
+++ public_html/php/classes/data-downloader.php
@@ -1,7 +1,9 @@
 <?php
 
 require_once "autoload.php";
-require_once("/ect/apache2/mysql/encrypted-config.php");
+require_once("/etc/apache2/mysql/encrypted-config.php");
+
+use Symm\Gisconverter\Gisconverter;
 
 /**
  * This class will download date from the City of Albuquerque Open-Trails Database.
@@ -10,14 +12,14 @@ require_once("/ect/apache2/mysql/encrypted-config.php");
  * @author Matt Harris mattharr505@gmail.com
  *
 **/
-class DataDownloader{
+class DataDownloader {
 
 	/**
 	 * named trails:http://data.cabq.gov/community/opentrails/named_trails.csv
 	 * stewards: http://data.cabq.gov/community/opentrails/stewards.csv
 	 * trail segments: http://data.cabq.gov/community/opentrails/trail_segments.geojson
 	 *
-	**/
+	 **/
 
 	/**
 	 * Gets the metadata from a file url
@@ -26,8 +28,8 @@ class DataDownloader{
 	 * @param int $redirect whether to redirect or not
 	 * @return mixed stream data
 	 * @throws Exception if file doesn't exist.
-	**/
-	public static function getMetaData ($url, $redirect = 1) {
+	 **/
+	public static function getMetaData($url, $redirect = 1) {
 		$context = stream_context_create(array("http" => array("follow_location" => $redirect, "ignore_errors" => true, "method" => "HEAD")));
 
 		//"@" suppresses warnings and errors
@@ -40,10 +42,10 @@ class DataDownloader{
 
 		$wrapperData = $streamData["wrapper_data"];
 
-		//loop through and find the "HTTP" atribute
-		$http ="";
-		foreach ($wrapperData as $data) {
-			if(strpos($data, "HTTP") !== false){
+		//loop through and find the "HTTP" attribute
+		$http = "";
+		foreach($wrapperData as $data) {
+			if(strpos($data, "HTTP") !== false) {
 				$http = $data;
 				break;
 			}
@@ -61,7 +63,7 @@ class DataDownloader{
 		if(strpos($http, "404")) {
 			throw (new Exception("Not Found"));
 		}
-		if(strpos($http,"418")) {
+		if(strpos($http, "418")) {
 			throw(new Exception("Get your tea set"));
 		}
 
@@ -73,7 +75,7 @@ class DataDownloader{
 	 *
 	 * @param string $url url to check
 	 * @return string "Last-Modified" attribute
-	**/
+	 **/
 	public static function getLastModified($url) {
 		// get the stream data
 		$streamData = DataDownloader::getMetaData($url);
@@ -99,7 +101,7 @@ class DataDownloader{
 	 * @param string $url url to check
 	 * @return DateTime date last modified
 	 **/
-	public static function getLastModifiedDate ($url) {
+	public static function getLastModifiedDate($url) {
 		// get the "Last-Modified" attribute
 		$lastModified = DataDownloader::getLastModified($url);
 		$dateString = null;
@@ -123,11 +125,11 @@ class DataDownloader{
 	 * @param string $path path to file
 	 * @param string $name filename
 	 * @param string $extension extension of the file
-	**/
+	 **/
 	public static function deleteFiles($path, $name, $extension) {
 		//Delete file(s)
 		$files = glob("$path$name*$extension");
-		foreach($files as $file){
+		foreach($files as $file) {
 			unlink($file);
 		}
 	}
@@ -140,14 +142,14 @@ class DataDownloader{
 	 * @param string $extension extension of stored file
 	 * @return DateTime date of stored file
 	 * @throws Exception if file does not exist
-	**/
+	 **/
 
 	public static function getDateFromStoredFile($path, $name, $extension) {
 		//get date from stored file
 		$currentDateStr = null;
 		$currentFile = null;
-		$files = glob ("$path$name*$extension");
-		if (count($files) > 0) {
+		$files = glob("$path$name*$extension");
+		if(count($files) > 0) {
 			$currentFile = $files[0];
 		} else {
 			return DateTime::createFromFormat("U", "0");
@@ -163,6 +165,7 @@ class DataDownloader{
 
 		return $currentDate;
 	}
+
 	/**
 	 * Downloads a file to a path from a url
 	 *
@@ -170,9 +173,9 @@ class DataDownloader{
 	 * @param string $path path to save to
 	 * @param string $name filename to save in
 	 * @param string $extension extension to save in
-	**/
+	 **/
 
-	public static function downloadFile ($url, $path, $name, $extension) {
+	public static function downloadFile($url, $path, $name, $extension) {
 		//delete old file(s)
 		DataDownloader::deleteFiles($path, $name, $extension);
 
@@ -197,85 +200,91 @@ class DataDownloader{
 		}
 	}
 
-		/**
-		 *This function grabs the named_trails.csv file and reads it
-		 *
-		 *@param string $urlBegin beginning of url to grab file at
-		 *@param string $urlEnd end of url to grab file at
-		 *@throws PDOException PDO related errors
-		 *@throws Exception catch-all exception
-		**/
-		public static function readNamedTrailsCSV($urlBegin, $urlEnd) {
-			$urls = glob("$urlBegin*$urlEnd");
-			if(count($urls) >0) {
-				$url = $urls[0];
-			}
+	/**
+	 *This function grabs the named_trails.csv file and reads it
+	 *
+	 * @param string $urlBegin beginning of url to grab file at
+	 * @param string $urlEnd end of url to grab file at
+	 * @throws PDOException PDO related errors
+	 * @throws Exception catch-all exception
+	 **/
+	public static function readNamedTrailsCSV($urlBegin, $urlEnd) {
+		$urls = glob("$urlBegin*$urlEnd");
+		if(count($urls) > 0) {
+			$url = $urls[0];
+		}
 
-			$context = stream_context_create(array("http" => array("ignore_errors" => true, "method" =>"GET")));
+		$context = stream_context_create(array("http" => array("ignore_errors" => true, "method" => "GET")));
 
-			try {
-				$pdo = connectToEncryptedMySQL("/ect/apache2/mysql/trailquail.ini");
+		try {
+			$pdo = connectToEncryptedMySQL("/ect/apache2/mysql/trailquail.ini");
 
-				if(($fd = @fopen ($url,"rb", false, $context)) !== false) {
-					fgetcsv($fd, 0 , "'");
-					while ((($data = fgetcsv($fd, 0 , "'")) !== false) && feof($fd) === false) {
-						$trailId = null;
-						$userId = "";
-						$browser = "";
-						$createDate = "";
-						$ipAddress ="";
-						$submitTrailId = "";
-						$trailAmenities = "";
-						$trailCondition = "";
-						$trailDescription = $data[3];
-						$trailDifficulty = "" ;
-						$trailDistance = "";
-						$trailName = $data[1];
-						$trailSubmissionType = "";
-						$trailTerrain = "";
-						$trailUse = "";
-						$trailUuid = "";
+			if(($fd = @fopen($url, "rb", false, $context)) !== false) {
+				fgetcsv($fd, 0, "'");
+				while((($data = fgetcsv($fd, 0, "'")) !== false) && feof($fd) === false) {
+					$trailId = null;
+					$userId = "";
+					$browser = "";
+					$createDate = "";
+					$ipAddress = "";
+					$submitTrailId = "";
+					$trailAmenities = "";
+					$trailCondition = "";
+					$trailDescription = $data[3];
+					$trailDifficulty = "";
+					$trailDistance = "";
+					$trailName = $data[1];
+					$trailSubmissionType = "";
+					$trailTerrain = "";
+					$trailUse = "";
+					$trailUuid = "";
 
-						//convert fields to UTF-8
-						$trailDescription = mb_convert_encoding($trailDescription, "UTF-8" );
-						$trailName = mb_convert_encoding($trailName, "UTF-8" );
 
-						try {
-							$trail = new Trail($trailName, $userId, $browser, $createDate, $ipAddress, $submitTrailId, $trailAmenities, $trailCondition, $trailDescription, $trailDifficulty, $trailDistance, $trailName, $trailSubmissionType, $trailTerrain, $trailUse, $trailUuid);
-							$trail = insert($pdo);
-						} catch (PDOException $pdoException) {
-							$sqlStateCode = "23000";
+					//convert fields to UTF-8
+					$trailDescription = mb_convert_encoding($trailDescription, "UTF-8");
+					$trailName = mb_convert_encoding($trailName, "UTF-8");
 
-							$errorInfo = $pdoException->errorInf;
-							if($errorInfo [0] === $sqlStateCode ) {
+					try {
+						$trail = new Trail($trailName, $userId, $browser, $createDate, $ipAddress, $submitTrailId, $trailAmenities, $trailCondition, $trailDescription, $trailDifficulty, $trailDistance, $trailName, $trailSubmissionType, $trailTerrain, $trailUse, $trailUuid);
+						$trail = insert($pdo);
+					} catch(PDOException $pdoException) {
+						$sqlStateCode = "23000";
+
+						$errorInfo = $pdoException->errorInf;
+						if($errorInfo [0] === $sqlStateCode) {
 						} else {
 							throw (new PDOException($pdoException->getMessage(), 0, $pdoException));
-							}
-						} catch (Exception $exception){
-							throw (new Exception ($exception->getMessage(), 0, $exception));
 						}
+					} catch(Exception $exception) {
+						throw (new Exception ($exception->getMessage(), 0, $exception));
+					}
 				}
 				fclose($fd);
 			}
 		} catch(PDOException $pdoException) {
 			throw (new PDOException($pdoException->getMessage(), 0, $pdoException));
-		} catch (Exception $exception) {
+		} catch(Exception $exception) {
 			throw(new Exception ($exception->getMessage(), 0, $exception));
 		}
 	}
 
 	/**
+	 *
+	 * Slurp Slurp
 	 * Decodes geoJson file, converts to string, sifts through the string and inserts the data into the database
+	 *
+	 * based on http://www.bewebdeveloper.com/tutorial-about-how-to-parse-json-file-with-php and the knowledge bequeathed
+	 * by the beloved Scrum Masters.
 	 *
 	 *
 	 * @param string $urlBegin beginning of url to grab file at
 	 * @param string $urlEnd end of url to grab file at
 	 * @throws PDOException PDO related errors
 	 * @throws Exception catch-all exception
-	**/
-	public static function readTrailSegmentsGeoJson($urlBegin, $urlEnd){
-		$urls = glob ("$urlBegin*$urlEnd");
-		if(count($urls) >0) {
+	 **/
+	public static function readTrailSegmentsGeoJson($urlBegin, $urlEnd) {
+		$urls = glob("$urlBegin*$urlEnd");
+		if(count($urls) > 0) {
 			$url = $urls[0];
 		} else {
 			throw(new RuntimeException("No file exists at specified location"));
@@ -285,6 +294,30 @@ class DataDownloader{
 		try {
 			$pdo = connectToEncryptedMySQL("/etc/apache2/mysql/trailquail.ini");
 
+			if(($jsonData = file_get_contents($url, null, $context)) === false) {
+
+				if(($jsonFd = @fopen("php://memory", "wb+")) === false) {
+					throw(new RuntimeException("Memory Error: I can't remember"));
+				}
+
+				//decode the geoJson file
+				$jsonConverted = json_decode($jsonData, true);
+
+				//copy the values to arrays
+				$jsonSegments = $jsonConverted->features->geometry->coordinates[0];
+				$jsonProperties = $jsonConverted->features->properties;
+				foreach($jsonSegments as $segment) {
+					this->$segment->
+
+				}
+				foreach($jsonProperties as $property){
+					this->$property->
+				}
+
+
+
+
+			}
 		}
 	}
 }
