967f94ca6e6d2bbdc862bb76cd9463d011f80cbf
George Kephart <Gkephart@cnm.edu>
2015-12-03 16:46:14 -0700

    https://app.asana.com/0/61036105436346/69245087519159 trail uuid can be left null

--- public_html/php/api/segment/index.php
+++ public_html/php/api/segment/index.php
@@ -4,7 +4,7 @@ require_once dirname(dirname(__DIR__)) . "/classes/autoload.php";
 require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
 // go over with dylan to make sure this is correct
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-require_once(dirname(dirname(dirname(__DIR__))) . "/vendor/autoload.php");
+require_once dirname(dirname(dirname(dirname(__DIR__)))) . "/vendor/autoload.php";
 /**
  * API for segment for segment class
  *
