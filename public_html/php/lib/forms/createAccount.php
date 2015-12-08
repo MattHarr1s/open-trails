58d34b95158a620ba4530aaf4e75c69d0d9fdf92
Jeff Saul <scaleup13@gmail.com>
2015-12-08 09:33:34 -0700

    Starting work on create account https://app.asana.com/0/61036105436352/63522445646889

---
+++ public_html/php/lib/forms/createAccount.php
@@ -0,0 +1,55 @@
+<!-------------------------------------------------------->
+<!-- This is the create-account form for the Trail Quail website -->
+<!-- 																	  -->
+<!-- @author saulj@me.com  (December 2015)  				  -->
+<!-------------------------------------------------------->
+
+<!-- The div class="form-wrap" is the black box containing the form. It's set to a column width of 12 for small screens, and a column width of 6 for medium screens on up -->
+<div class="form-wrap">
+
+	<!-- Form is centered within it's container, and is set to 10 be columns wide RELATIVE TO IT'S CONTAINER, and offset to the right by one column. See classes: col-xs-offset-1 & col-xs-10 -->
+	<form method="get" action="#" id="login-form" class="form-horizontal">
+
+		<div class="form-group">
+			<!-- Labels for each field are places within a <label> tag. Use the "for" attribute. the class="control-label" is for styling. -->
+			<label for="inputUserName1" class="control-label">Username</label>
+			<!-- the div class="input-group" contains both the text field and the icon to the left -->
+			<div class="input-group">
+				<!-- this div and span contains the glyphicon to the left. aria-hidden is so that screen readers don't read this element -->
+				<div class="input-group-addon">
+					<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+				</div>
+				<!-- text field input. pay attention to the id, placeholder text, type, and placeholder attributes -->
+				<input type="text" class="form-control" id="UserName1" placeholder="Your username here." maxlength="150"/>
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label for="inputPassword1" class="control-label">Password</label>
+			<div class="input-group">
+				<div class="input-group-addon">
+					<span class="glyphicon glyphicon-lock" aria-hidden="true"></span>
+				</div>
+				<input type="password" id="password1" class="form-control" maxlength="150"
+						 placeholder="your password"/>
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label for="inputEmail1" class="control-label">Email</label>
+			<div class="input-group">
+				<div class="input-group-addon">
+					<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+				</div>
+				<input type="Email" id="email1" class="form-control" maxlength="150"
+						 placeholder="your-email@something.com"/>
+			</div>
+		</div>
+
+		<!-- buttons for submit and reset -->
+		<hr>
+		<button class="btn btn-md btn-info" type="submit">Submit</button>
+		<button class="btn btn-md btn-warning" type="reset">Reset</button>
+
+	</form>
+</div> <!-- CLOSE FORM WRAP -->
