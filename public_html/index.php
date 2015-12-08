b962ae5573274346c2e2be4ecb51364bd9ab0abb
Jeff Saul <scaleup13@gmail.com>
2015-12-08 13:54:41 -0700

    templating with Rochelle - landing page templated https://app.asana.com/0/61036105436352/63522445646889

--- public_html/index.php
+++ public_html/index.php
@@ -41,11 +41,7 @@ require_once("php/template/head-utils.php");
 </div>
 
 <footer class="footer">
-	<div class="container">
-		<p class="text-muted">Copyright Comments<span class="pull-right"><a class="btn btn-primary" href="#"
-																								  role="button">Contact Us</a></span>
-		</p>
-	</div>
+	<?php require_once("php/template/footer.php"); ?>
 </footer>
 
 </body>
---
+++ public_html/php/template/footer.php
@@ -0,0 +1,6 @@
+
+	<div class="container">
+		<p class="text-muted">Copyright Comments<span class="pull-right"><a class="btn btn-primary" href="#"
+																								  role="button">Contact Us</a></span>
+		</p>
+	</div>
