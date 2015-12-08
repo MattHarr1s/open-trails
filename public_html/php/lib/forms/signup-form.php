74a92ba207c793b89a17305821232a73ca37c0f6
Louis Gill <lgill7@cnm.edu>
2015-12-08 11:58:42 -0700

    lol

--- public_html/php/lib/forms/signup-form.php
+++ public_html/php/lib/forms/signup-form.php
@@ -7,7 +7,7 @@
 <!-- The div class="form-wrap" is the black box containing the form. It's set to a column width of 6 for medium screens on up -->
 <div class="col-xs-12 col-md-7 form-wrap">
 	<!-- Form is centered within its container, and is set to be 10 columns wide RELATIVE TO ITS CONTAINER, and offset to the right by one column. See classes: col-xs-offset-1 & col-xs-10 -->
-	<form method="get" action="#" id="sample-form" class="form-horizontal" col-xs-10 col-xs-offset-1">
+	<form method="get" action="" id="sample-form" class="form-horizontal" col-xs-10 col-xs-offset-1">
 		<div class="form-group">
 			<!-- Labels for each field are places within a <label> tag. Use the "for" attribute. the class="control-label" is for styling. -->
 			<label for="userName" class="control-label">user name</label>
---
+++ public_html/php/lib/forms/trail-alert-form.php
@@ -0,0 +1,17 @@
+<!-- trail alert modal -->
+<div class="modal fade" id="alert" tabindex="-1" role="dialog" aria-labelledby="alert">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h3 class="modal-title" id="alert">Alert!</h3>
+			</div>
+			<div class="modal-body">
+				<form id="alert" name="alert" class="form" action="<?php echo $PREFIX?>"												method="post"	>// ????????????????
+					<div class="form-group">
+						<label for="commentText">alert text</label>
+						<input type="text" class="form-control" id="commentText" name="commentText" placeholder="Is there a pressing issue with the trail? Tell us here."
+			</div>
+		</div>
+	</div>
+</div>
