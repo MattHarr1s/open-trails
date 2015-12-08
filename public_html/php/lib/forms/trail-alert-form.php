668508dd0110bb357232853bfa16be9fef3bba32
Louis Gill <lgill7@cnm.edu>
2015-12-08 15:36:33 -0700

    done with alert form?

--- public_html/php/lib/forms/trail-alert-form.php
+++ public_html/php/lib/forms/trail-alert-form.php
@@ -7,10 +7,21 @@
 				<h3 class="modal-title" id="alert">Alert!</h3>
 			</div>
 			<div class="modal-body">
-				<form id="alert" name="alert" class="form" action="<?php echo $PREFIX?>"												method="post"	>// ????????????????
+				<form id="alert" name="alert" class="form" action="<?php echo $PREFIX?>php/controllers/sign-up-controller.php" method="post">												// ????????????????
 					<div class="form-group">
 						<label for="commentText">alert text</label>
-						<input type="text" class="form-control" id="commentText" name="commentText" placeholder="Is there a pressing issue with the trail? Tell us here."
+						<input type="text" class="form-control" id="commentText" name="commentText" placeholder="Is there something wrong with the trail? Tell us here."
+					</div>
+					<div class="form-group">
+						<label for="commentPhoto">upload photo</label>
+						<input type="commentPhoto" class="form-control" id="commentPhoto" name="commentPhoto" placeholder="upload photo">														// HOW TO DO PHOTO???????????
+					</div>
+					<div class="form-group">																																												// FORM GROUP?????????????????
+							<button type="submit" class="btn modal-button" id="submitButton" name="submitButton">Submit Alert</button>
+																																																										// TRUFORK????????????????????
+					</div>
+				</form>
+				<div id="outputArea"></div>																																													// ?????????????????????????
 			</div>
 		</div>
 	</div>
