e7361810a69582a080b8e7e9a79ab6ad3b45b41b
Louis Gill <lgill7@cnm.edu>
2015-12-10 11:46:46 -0700

    login-controller.js

--- public_html/angular/controllers/login-controller.js
+++ public_html/angular/controllers/login-controller.js
@@ -1,4 +1,32 @@
+app.controller("LoginController", ["$scope", "uibModal", "LoginService", function($scope, $uibModal, LoginService) {
+	$scope.loginData = {};
 
+	$scope.openLoginModal = function () {
+		var loginModalInstance = $uibModal.open({
+			templateUrl: "angular/views/create-account-form.php",
+			controller: "LoginModal",
+			resolve: {
+				loginData: function() {
+					return($scope.loginData);
+				}
+			}
+		});
+		loginModalInstance.result.then(function(loginData) {
+			$scope.loginData = loginData;
+			LoginService.login(loginData)
+				.then(function(reply) {
+					if(reply.status === 200) {
+						AlertService.addAlert({type: "success", msg: reply.message});
+						$window.location.reload();
+					} else {
+						AlertService.addAlert({type: "danger", msg: reply.message});
+					}
+				});
+		}, function() {
+			$scope.loginData = {};
+		});
+	};
+}]);
 
 
 
