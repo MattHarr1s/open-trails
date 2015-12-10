90306820258c735ef74a5647e5c26f5697e5627f
Louis Gill <lgill7@cnm.edu>
2015-12-10 13:37:55 -0700

    login-service.js

---
+++ public_html/angular/services/login-service.js
@@ -0,0 +1,10 @@
+app.service("LoginService", function($http) {
+	this.LOGIN_ENDPOINT = "/auth/login";
+
+	this.login = function(loginData) {
+		return($http.post(this.LOGIN_ENDPOINT, loginData)
+			.then(function(reply) {
+				return(reply.data);
+			}));
+	};
+});
