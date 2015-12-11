dc516e80f944dd025f05ef1e8a03e211b67b3e01
Louis Gill <lgill7@cnm.edu>
2015-12-10 20:42:07 -0700

    ADDED PASSWORD TO CREATE-ACCOUNT-MODAL.PHP

--- public_html/angular/views/create-account-modal.php
+++ public_html/angular/views/create-account-modal.php
@@ -20,6 +20,17 @@
 				<p ng-message="required">Please enter a user name</p>
 			</div>
 
+		<!-- PASSWORD -->
+			<label class="control-label sr-only" for="password">Password</label>
+
+			<div class="input-group">
+				<input type="text" class="form-control" id="password" name="password" placeholder="password" ng-model="signupData.password" ng-required="true"/>
+			</div>
+			<div class="alert alert-danger" role="alert" ng-messages="signupData.password.$error" ng-if="signupData.password.$touched" ng-hide="signupData.password.$valid">
+				<p ng-message="required">Please enter a password</p>
+			</div>
+
+			<!-- VERIFY PASSWORD -->
 
 		</div>
 	</form>
