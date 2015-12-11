3f0727b4f040b15d78e3c540cc810ecf538a6acd
Louis Gill <lgill7@cnm.edu>
2015-12-11 16:06:24 -0700

    started comment-controller.js

--- public_html/angular/controllers/comment-controller.js
+++ public_html/angular/controllers/comment-controller.js
@@ -1,2 +1,21 @@
+app.controller("CommentController", ["$scope", "$uibModal", "SegmentService", function($scope, $uibModal, SegmentService) {
+	$scope.segments = [];
+	$scope.alerts = [];
+	$scope.numSegments = 2;
 
+	$scope.getCommentId = function(commentId, validated) {
+		if (validated === true) {
+			CommentService.fetchId(commentId)
+				.then(function(result) {
+					if(result.data.status === 200) {
+						$scope.comments = result.data.data
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message}
+					}
+				});
+		}
+	}
+};
 
+	$scope.getComment
+])
