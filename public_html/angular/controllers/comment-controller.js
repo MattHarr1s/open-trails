fbdea79bf4872d5005e59500ca0fc032b8d39595
Louis Gill <lgill7@cnm.edu>
2015-12-11 16:26:03 -0700

    added commentbytrailid & userId

--- public_html/angular/controllers/comment-controller.js
+++ public_html/angular/controllers/comment-controller.js
@@ -17,5 +17,40 @@ app.controller("CommentController", ["$scope", "$uibModal", "SegmentService", fu
 	}
 };
 
-	$scope.getComment
+	$scope.getComments = function() {
+		CommentService.all()
+			.then(function(result) {
+				if(result.data.status === 200) {
+					$scope.organizations = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			});
+	};
+
+	$scope.getCommentByTrailId = function(trailId, validated) {
+		if(validated === true) {
+			CommentService.fetchTrailId(trailId)
+				.then(function(result) {
+					if(result.data.status === 200) {
+						$scope.comments = result.data.data;
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				});
+		}
+	};
+
+	$scope.getCommentByUserId = function(userId, validated) {
+		if(validated === true) {
+			CommentService.fetchUserId(userId)
+				.then(function(result) {
+					if(result.data.status === 200) {
+						$scope.comments = result.data.data;
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				});
+		}
+	};
 ])
