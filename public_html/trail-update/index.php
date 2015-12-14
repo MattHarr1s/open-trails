4480305be8fd156ab1b82b71758e75f8574cdbb6
Jeff Saul <scaleup13@gmail.com>
2015-12-14 08:33:50 -0700

    began updating trail update web page
    https://app.asana.com/0/61036105436352/63522445646889

--- public_html/trail-update/index.php
+++ public_html/trail-update/index.php
@@ -2,7 +2,7 @@
 /*grab current directory*/
 $CURRENT_DIR = __DIR__;
 /*set page title here*/
-$PAGE_TITLE = "MY PAGE TITLE";
+$PAGE_TITLE = "Trail Update";
 /*load head-utils.php*/
 require_once("../php/template/head-utils.php");
 ?>
@@ -13,51 +13,75 @@ require_once("../php/template/head-utils.php");
 		<?php require_once(dirname(__DIR__) . "/php/template/header.php"); ?>
 	</header>
 
-	<h1>Trail Description</h1>
+	<h1>Make Trail Corrections</h1>
+	<div>Help us fix our trail information</div>
+	<!-- trail database updates webpage - super user access only -->
 	<hr>
 
+	<!-- current database entry on trail -->
 	<div class="container">
 		<div class="row">
 			<!--map container-->
-			<div class="col-md-6 embed-responsive embed-responsive-4by3">
-				<div id="map"></div>
-			</div>
-			<!--data column-->
 			<div class="col-md-6">
-				<div>Trail name here</div>
+				<h2>Current Trail DB Entry</h2>
+
+				<!--	Will need to define variables for currentTrailId, antiabuse trait, currentTrailName, currentTrailDistance, currentTrailDifficulty, currentTrailUse, and currentTrailDescription	-->
+
+				<!-- Dummy data for now -->
+				<!--					$trailName = "Corrales Bosque Trail";-->
+				<!--					$trailDistance = 13.0;-->
+				<!--					$trailDifficulty = 1;-->
+				<!--					$trailUse[] = [1, 1, 0, 0, 1];-->
+				<!--					$trailDescription = "Located in northern Albuquerque, the Corrales Bosque Trail offers a quick escape nearby. The trail offers scenic views of the Rio Grande.  It also offers opportunities for birding and wildlife viewing. The trail is paved at the beginning turning into a dirt and sand singletrack on a flat wooded trail along the Rio Grande. Restrooms available at the Alameda Open Space parking lot (cross the pedestrian bridge over the river & then go under Alameda to get to the parking lot from the trailhead.";-->
+				<!--					-->
+
+				<h3>Corrales Bosque Trail</h3>
+				<div class="row">
+					<div class="col-md-10 embed-responsive embed-responsive-4by3">
+						<div id="map"></div>
+					</div>
+					<!-- End of Map row / beginning of trail info -->
+				</div>
+				<div>The information below is what is currently in the database:</div>
+				<!-- trail difficulty goes here as a row -->
+				<div class="row"></div>
+				<div>Trail Difficulty goes here</div>
+				<div class="row"></div>
+				<div>Minimum Trail Distance goes here</div>
+				<div class="row"></div>
+				<div>Trail Use goes here</div>
 				<br>
-				<div>Trail Search Parameters go here</div>
+
+				<div class="row"></div>
+				<h3>Trail Description:</h3>
+				<div>Located in northern Albuquerque, the Corrales Bosque Trail offers a quick escape nearby. The trail offers scenic views of the Rio Grande.  It also offers opportunities for birding and wildlife viewing. The trail is paved at the beginning turning into a dirt and sand singletrack on a flat wooded trail along the Rio Grande. Restrooms available at the Alameda Open Space parking lot (cross the pedestrian bridge over the river & then go under Alameda to get to the parking lot from the trailhead.)</div>
+			</div> <!-- End column 1 here -->
+			<!--Correction/New Trail data column-->
+
+			<div class="col-md-6">
+				<h2>Trail Correction(s)</h2>
+
+				<h3>Corrales Bosque Trail</h3>
+				<!--	Note that all current trail information will be copied into update fields	-->
+				<div class="row">
+					<div class="col-md-10 embed-responsive embed-responsive-4by3">
+						<div id="map2"></div>
+					</div>
+					<!-- End of Map row / beginning of trail info -->
+				</div>
+				<?php require_once(dirname(__DIR__) . "/angular/views/trail-update-form.php"); ?>
 				<br>
-				<button class="btn btn-md btn-info" type="submit">Trail Corrections</button>
-				<button class="btn btn-md btn-warning" type="reset">Trail Alert</button>
-				<!--continue to fill in content next to map here-->
-			</div>
-		</div><!--.row-->
 
-		<!-- Trail comment form inserted here -->
-		<div class="row">
-			<div class="col-md-12">
-				<h2 class="page-title">Enter Your Comment(s) for this Trail</h2>
-				<?php require_once(dirname(__DIR__) . "/angular/views/comment-form.php"); ?>
-			</div>
-		</div>
-		<br>
-		<hr>
-
-		<!-- Trail comments from database will show below here -->
-		<div class="row">
-			<div class="col-md-12">
-				List of trail commments here
-			</div>
-		</div>
+				<!-- End of column 2 -->
+			</div><!--.row-->
 
 
-	</div><!--.container-->
-</div>
+		</div><!--.container-->
+	</div>
 
-<footer class="footer">
-	<?php require_once(dirname(__DIR__) . "/php/template/footer.php"); ?>
-</footer>
+	<footer class="footer">
+		<?php require_once(dirname(__DIR__) . "/php/template/footer.php"); ?>
+	</footer>
 
-</body>
-</html>
+	</body>
+	</html>
