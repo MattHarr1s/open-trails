90d2a3b6f66355b39ca4d81c54cdcf46e48c02da
Louis Gill <lgill7@cnm.edu>
2015-12-14 13:20:01 -0700

    started route-config.js

--- public_html/angular/route-config.js
+++ public_html/angular/route-config.js
@@ -4,7 +4,25 @@ trailQuail.config(function($routeProvider) {
 
 	// route for the trail page
 		.when('/', {
-			templateUrl: '',
-			controller: ''
+			templateUrl: '../../trail-info/index.php',
+			controller: 'TrailController'
 		})
-})
+
+		// route for comments
+		.when('/', {
+			templateUrl: '../../trail-info/index.php',
+			controller: 'CommentController'
+		})
+
+		// route for segments
+		.when('/', {
+			templateUrl: '../../trail-info/index.php',
+			controller: 'SegmentController'
+		});
+});
+
+// create the controller and inject Angular's $scope
+trailQuail.controller('TrailController', function($scope) {
+	// create a message to display in our view
+	$scope.message = 'Blah blah blah';
+});
