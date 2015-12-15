5eb5df8378842b5ccbec036945108560845a0379
Matt Harris <mateodelay@gmail.com>
2015-12-15 10:35:24 -0700

    Changing map type on Index.php

--- public_html/index.php
+++ public_html/index.php
@@ -38,20 +38,7 @@ require_once("php/template/head-utils.php");
 	<div class="container">
 		<div class="row">
 			<div class	="col-md-12 embed-responsive embed-responsive-4by3">
-				<ng-map zoom="3" center="0, -180" map-type-id="TERRAIN">
-					<shape name="polyline"
-							 path="[
-        [37.772323, -122.214897],
-        [21.291982, -157.821856],
-        [-18.142599, 178.431],
-        [-27.46758, 153.027892]
-      ]"
-							 geodesic="true"
-							 stroke-color="#FF0000"
-							 stroke-opacity="1.0"
-							 stroke-weight="2">
-					</shape>
-				</ng-map>
+				<ng-map zoom="8" center="35.1318, -106.5925" map-type-id="TERRAIN"></ng-map>
 			</div>
 
 		</div>
